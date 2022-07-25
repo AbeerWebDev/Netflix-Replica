bd9d7e6751b562151dd4b573c738386c8f73c3d7
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-25 18:47:37 +0400

    Working on navbar

--- myapp/src/home/Home.jsx
+++ myapp/src/home/Home.jsx
@@ -1,10 +1,250 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Aspernatur asperiores sed illo, suscipit porro architecto assumenda magni
+      maiores, doloremque, dicta odit reiciendis. Modi mollitia quisquam
+      voluptas eius nulla provident. Eaque. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Aspernatur asperiores sed illo, suscipit
+      porro architecto assumenda magni maiores, doloremque, dicta odit
+      reiciendis. Modi mollitia quisquam voluptas eius nulla provident. Eaque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+      asperiores sed illo, suscipit porro architecto assumenda magni maiores,
+      doloremque, dicta odit reiciendis. Modi mollitia quisquam voluptas eius
+      nulla provident. Eaque. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Aspernatur asperiores sed illo, suscipit porro
+      architecto assumenda magni maiores, doloremque, dicta odit reiciendis.
+      Modi mollitia quisquam voluptas eius nulla provident. Eaque. Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Aspernatur asperiores sed
+      illo, suscipit porro architecto assumenda magni maiores, doloremque, dicta
+      odit reiciendis. Modi mollitia quisquam voluptas eius nulla provident.
+      Eaque.
+    </div>
+  );
+};
 
-export default Home
+export default Home;
