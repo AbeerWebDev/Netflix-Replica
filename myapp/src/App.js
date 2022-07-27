9e7bb76626818e8378a3ea7976ff474cecbb6a99
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-27 13:40:53 +0400

    Finished project UI

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,13 @@
 import "./app.scss";
-import Home from "./pages//home/Home";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="app">
-      <Register />
-      {/* <Home /> */}
-      {/* <Watch /> */}
+      <Login />
     </div>
   );
 }
