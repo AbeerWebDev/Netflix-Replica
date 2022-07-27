b8c620174dd927df1ac8ba084fea2c3f8ef6e0aa
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-27 12:33:38 +0400

    Finished register page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,14 @@
 import "./app.scss";
 import Home from "./pages//home/Home";
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div className="app">
+      <Register />
       {/* <Home /> */}
-      <Watch />
+      {/* <Watch /> */}
     </div>
   );
 }
--- myapp/src/pages/home/Home.jsx
+++ myapp/src/pages/home/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
       <List />
       <List />
       <List />
-      <List />
      </div>
   );
 };
